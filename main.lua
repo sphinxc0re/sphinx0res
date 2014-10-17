cb8811309b6aaed108f2b920352104c28445a00c
Julian Laubstein <julianlaubstein@yahoo.de>
2014-10-17 23:26:12 +0200

    Added functioning obfuscation algorithm and saving of chunks

--- main.lua
+++ main.lua
@@ -50,42 +50,20 @@ end
 
 
 function OnChunkGenerated(World, ChunkX, ChunkZ, ChunkDesc)
-  Chunk0 = cBlockArea()
-  ChunkDesc:ReadBlockArea(Chunk0, 0, 14, 0, (ChunkDesc:GetMaxHeight()), 0, 14)
-  -- LOG("READ CHUNK")
-  for y = 0, ChunkDesc:GetMaxHeight() do
-    for x = 0, 14 do
-      for z = 0, 14 do
-        local btype = Chunk0:GetBlockType(x, y, z)
-        for n = 0, g_NumOres do
-          if btype == g_Ores[n] then
-            -- LOG("Ore found at " .. x .. " " .. y .. " " .. z .. " ")
-            World:ForEachPlayer(
-              function(Player)
-                local ClientHandle = Player:GetClientHandle()
-                if not HasAir(World, x, y, z) then
-                  Chunk0:SetBlockType(x, y, z, 0)
-                end
-              end
-            )
-          end
-        end
-      end
-    end
-  end
+  ObfuscateChunk(ChunkX, ChunkZ, ChunkDesc)
 end
 
 
 
-
-function HasAir(World, X, Y, Z)
+-- Thanks to STR_Warrior for this function
+function HasAir(BlockArea, X, Y, Z)
   if (
-    cBlockInfo:IsTransparent(World:GetBlock(X + 1, Y, Z)) or
-    cBlockInfo:IsTransparent(World:GetBlock(X - 1, Y, Z)) or
-    cBlockInfo:IsTransparent(World:GetBlock(X, Y + 1, Z)) or
-    cBlockInfo:IsTransparent(World:GetBlock(X, Y - 1, Z)) or
-    cBlockInfo:IsTransparent(World:GetBlock(X, Y, Z + 1))or
-    cBlockInfo:IsTransparent(World:GetBlock(X, Y, Z - 1))) then
+    cBlockInfo:IsTransparent(BlockArea:GetRelBlockType(X + 1, Y, Z)) or
+    cBlockInfo:IsTransparent(BlockArea:GetRelBlockType(X - 1, Y, Z)) or
+    cBlockInfo:IsTransparent(BlockArea:GetRelBlockType(X, Y + 1, Z)) or
+    cBlockInfo:IsTransparent(BlockArea:GetRelBlockType(X, Y - 1, Z)) or
+    cBlockInfo:IsTransparent(BlockArea:GetRelBlockType(X, Y, Z + 1)) or
+    cBlockInfo:IsTransparent(BlockArea:GetRelBlockType(X, Y, Z - 1))) then
       return true
   end
     return false
--- obfuscator.lua
+++ obfuscator.lua
@@ -2,7 +2,20 @@
 ----- OBFUSCATOR ------------------
 -----------------------------------
 
-function Obfuscate(BlockArea)
-  
-  
+function ObfuscateChunk(ChunkX, ChunkZ, ChunkDesc)
+  ChunkBlockArea = cBlockArea()
+  ChunkDesc:ReadBlockArea(ChunkBlockArea, 0, 14, 0, (ChunkDesc:GetMaxHeight()), 0, 14)
+  -- LOG("READ CHUNK")
+  for RelY = 0, ChunkDesc:GetMaxHeight() do
+    for RelX = 0, 15 do
+      for RelZ = 0, 15 do
+        if not HasAir(ChunkBlockArea, RelX, RelY, RelZ) then
+          ChunkBlockArea:SetRelBlockType(RelX, RelY, RelZ, g_Ores[math.random(1, g_NumOres)])
+        end
+      end
+    end
+  end
+  cFile:CreateFolder("SporeSchema")
+  ChunkBlockArea:SaveToSchematicFile("SporeSchema/CHUNK#" .. ChunkX .. "#" .. ChunkZ .. ".schematic")
+  LOG("Saved obfuscated chunk ".. ChunkX .. "#" .. ChunkZ .." to drive!")
 end
